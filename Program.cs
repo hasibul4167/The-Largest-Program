fd30b21d20237cbc63b8493a5731976da9e2610c
Md Hasibul Islam <hasibulislam3409@gmail.com>
2022-03-22 11:09:07 +0600

    The Largest Program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿class  Program
+{
+    static void Main(string []arrys)
+    {
+        int first,second,third;
+        Console.WriteLine("Enter the First number");
+        first = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the Second number");
+        second = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the Third number");
+        third = Convert.ToInt32(Console.ReadLine());
+
+        if (first > second & first > third)
+        {
+            Console.WriteLine("The Largest Number - " + second);
+        }
+        else if (second > first && second > third)
+        {
+            Console.WriteLine("The Largest Number - " + second);
+        }
+        else 
+        {
+            Console.WriteLine("The Largest Number - " + third);
+        }
+        Console.WriteLine("The Code is End");
+    }
+
+
+}
